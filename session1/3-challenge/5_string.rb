8c4ffb8927e4234627bb182ae243a5788dad7a5c
Dom Batten <dominic.batten@googlemail.com>
2015-08-12 10:56:29 +0100

    Update 5_string.rb

--- session1/3-challenge/5_string.rb
+++ session1/3-challenge/5_string.rb
@@ -5,4 +5,14 @@
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
+	array = string.split()
+	array.map! { |x| x == "sad" ? "happy" : x}
+	array.map! { |x| x == "sad." ? "happy." : x}
+	array.map! { |x| x == "Sad" ? "Happy" : x}
+	newString = array.join(" ")
+	puts newString
 end
+
+add_more_ruby("The clowns were sad.") 
+add_more_ruby("The sad dad said sad stuff.")
+add_more_ruby("Sad times are ahead!")
