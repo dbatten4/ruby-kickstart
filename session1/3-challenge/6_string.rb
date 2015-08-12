2766982ba899e0debbe32f211979e3096cd0525a
Dom Batten <dominic.batten@googlemail.com>
2015-08-12 11:39:38 +0100

    Update 6_string.rb

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -6,5 +6,26 @@
 #
 
 def odds_and_evens(string, return_odds)
-
+	array = string.split(//)
+	toReturn = []
+	if return_odds == true
+		for i in 0..(string.length)
+			if i % 2 == 1
+				toReturn.push(array[i])
+			else
+			end
+		end
+	else
+		for i in 0..(string.length)
+			if i % 2 == 0
+				toReturn.push(array[i])
+			else
+			end
+		end
+	end
+	newString = toReturn.join(" ")
+	puts newString
 end
+
+odds_and_evens("hello my name is dominic", false)
+odds_and_evens("hello my name is dominic", true)
